0a566ffb0adb1f89101e7010bef2d244bd323e16
Blank <wangfj11@foxmial.com>
2024-01-17 20:28:53 +0800

    [修改]1. 修改编译警告

--- Runtime/Utility/CRC64Algorithm.cs
+++ Runtime/Utility/CRC64Algorithm.cs
@@ -81,7 +81,6 @@ namespace YooAsset
 
                 /// <summary>Gets the current computed hash value without modifying accumulated state.</summary>
                 /// <returns>The hash value for the data already provided.</returns>
-                [CLSCompliant(false)]
                 public ulong GetCurrentHashAsUInt64() => _crc;
 
                 /// <summary>
@@ -164,7 +163,6 @@ namespace YooAsset
                 /// <summary>Computes the CRC-64 hash of the provided data.</summary>
                 /// <param name="source">The data to hash.</param>
                 /// <returns>The computed CRC-64 hash.</returns>
-                [CLSCompliant(false)]
                 public static ulong HashToUInt64(ReadOnlySpan<byte> source) =>
                     Update(InitialState, source);
 
